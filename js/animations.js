e96d3579fa6cf5e7a25b242bd2fa387277b88141
Tom Roche <tomroche.development@gmail.com>
2023-11-08 21:26:14 +0100

    Add animation on header on upscroll

--- js/animations.js
+++ js/animations.js
@@ -17,3 +17,30 @@ for (let i = 0; i < sectionHeaders.length; i++) {
 
     observer.observe(element);
 }
+
+// Hide header on scrolldown
+const pageHeader = document.querySelector("header");
+
+document.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 100) {
+        pageHeader.classList.add("hide");
+    } else {
+        pageHeader.classList.remove("hide");
+    }
+});
+
+// Show header on upscroll
+let previousScrollY = 0;
+
+window.addEventListener("wheel", () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY < previousScrollY && document.documentElement.scrollTop > 100) {
+        // Code comes here
+        pageHeader.classList.add("show-bg-black");
+    } else {
+        pageHeader.classList.remove("show-bg-black");
+    }
+
+    previousScrollY = currentScrollY;
+});
