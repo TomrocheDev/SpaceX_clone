b3b3089a1f75980e2935a04ed1ebb0df4cb2603d
Tom Roche <tomroche.development@gmail.com>
2023-11-08 20:27:28 +0100

    Add new sections -- Add section header animation on scroll

---
+++ js/animations.js
@@ -0,0 +1,19 @@
+// Section header scroll-in animation
+const sectionHeaders = document.querySelectorAll(".fade-in-element");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("animate_fade-in-element");
+            }
+        });
+    },
+    { threshold: 0.5 }
+);
+
+for (let i = 0; i < sectionHeaders.length; i++) {
+    const element = sectionHeaders[i];
+
+    observer.observe(element);
+}
